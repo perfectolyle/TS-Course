a22355ec2dd60eadaed2405c2835e88cb2ded548
Perfecto <perfectogardoce@gmail.com>
2026-06-26 13:19:46 +0800

    This is Chapter 4: lesson about functios

--- build/js/main.js
+++ build/js/main.js
@@ -13,5 +13,41 @@ let subtract = function (a, b) {
     return a - b;
 };
 logMsg(subtract(1, 2));
+// interface mathFunction {
+//     (a: number, b: number): number;
+// }
+let multiply = (a, b) => {
+    return a * b;
+};
+logMsg(multiply(5, 10));
+// optional parameters
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+logMsg(addAll(1, 2));
+logMsg(addAll(1, 2, 3));
+// default parameters
+const sumAll = (a, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(sumAll(1, 2));
+logMsg(sumAll(1, 2, 3));
+// rest parameters
+const total = (...nums) => {
+    return nums.reduce((prev, current) => prev + current);
+};
+logMsg(total(1, 2, 3, 4, 10));
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+const numberOrString = (value) => {
+    if (typeof value === 'number') {
+        return 'number';
+    }
+    return 'string';
+};
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -35,3 +35,55 @@ let subtract = function (a: number, b: number) {
 
 logMsg(subtract(1, 2));
 
+type mathFunction = (a: number, b: number) => number;
+// interface mathFunction {
+//     (a: number, b: number): number;
+// }
+
+let multiply: mathFunction = (a, b) => {
+    return a * b;
+};
+
+logMsg(multiply(5, 10));
+
+
+// optional parameters
+
+const addAll = (a: number, b: number, c?: number) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+
+logMsg(addAll(1, 2));
+logMsg(addAll(1, 2, 3));
+
+// default parameters
+
+const sumAll = (a: number, b: number, c: number = 2) => {
+    return a + b + c;
+};
+
+logMsg(sumAll(1, 2));
+logMsg(sumAll(1, 2, 3));
+
+
+// rest parameters
+
+const total = (...nums: number[]) => {
+    return nums.reduce((prev, current) => prev + current);
+};
+
+logMsg(total(1, 2, 3, 4, 10));
+
+const createError = (errMsg: string) => {
+    throw new Error(errMsg);
+};
+
+const numberOrString = (value: number | string ): string => {
+    if (typeof value === 'number') {
+        return 'number';
+    }
+    return 'string';
+}
